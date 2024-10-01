ce7af2801806dd57db081af83a24058f6e768a80
Duc Anh <137094647+Konta37@users.noreply.github.com>
2024-10-01 17:21:25 +0700

    add

--- src/pages/user/home/review/index.jsx
+++ src/pages/user/home/review/index.jsx
@@ -1,57 +1,47 @@
 import React from "react";
-
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
-import 'swiper/css'
-import 'swiper/css/navigation'
+import "swiper/css";
+import "swiper/css/navigation";
+
 export default function Review() {
   return (
     <>
-      <section className="block p-0 w-full max-w-[1200px] m-[0_auto] z-20 mt-14 review-b mb-14 ">
-        <div className="w-full flex flex-col items-center justify-between  m-[0_auto] relative z-[10] ">
-          <Swiper 
+      <section className="block p-0 w-full max-w-[1200px] mx-auto z-20 mt-14 mb-14">
+        <div className="w-full flex flex-col items-center justify-between relative z-10">
+          <Swiper
             navigation={true}
             modules={[Navigation]}
-            className="mySwiper  rounded-2xl shadow-lg "
+            className="mySwiper rounded-2xl shadow-lg"
             loop={true}
           >
-            <SwiperSlide >
-              <div className="flex flex-col items-center justify-center text-center w-full bg-[#EBEBEB] ">
-                <p className="mt-8 mb-8 ">
+            <SwiperSlide>
+              <div className="flex flex-col items-center justify-center text-center w-full bg-gray-200">
+                <p className="mt-8 mb-8">
                   <i
-                    className="bi bi-chat-quote"
-                    style={{
-                      fontSize: "36px",
-                      marginRight: "10px",
-                      color: "blue",
-                    }}
+                    className="bi bi-chat-quote text-[36px] mr-[10px] text-blue-500"
                   ></i>
                 </p>
                 <p className="font-bold w-[800px]">
                   Being an avid language learner, I've always sought ways to
-                  practice and im-prove my skills. Minimalin has become my
-                  language learing companion.
+                  practice and improve my skills. Minimalin has become my
+                  language learning companion.
                 </p>
                 <p className="mt-8 mb-8">Emily Smith</p>
               </div>
             </SwiperSlide>
-            <SwiperSlide >
-              <div className="flex flex-col items-center justify-center text-center w-full bg-[#EBEBEB]">
-                <p className="mt-8 mb-8 ">
+            <SwiperSlide>
+              <div className="flex flex-col items-center justify-center text-center w-full bg-gray-200">
+                <p className="mt-8 mb-8">
                   <i
-                    className="bi bi-chat-quote"
-                    style={{
-                      fontSize: "36px",
-                      marginRight: "10px",
-                      color: "blue",
-                    }}
+                    className="bi bi-chat-quote text-[36px] mr-[10px] text-blue-500"
                   ></i>
                 </p>
                 <p className="font-bold w-[800px]">
                   Being an avid language learner, I've always sought ways to
-                  practice and im-prove my skills. Minimalin has become my
-                  language learing companion.
+                  practice and improve my skills. Minimalin has become my
+                  language learning companion.
                 </p>
                 <p className="mt-8 mb-8">Emily Smith</p>
               </div>
--- src/pages/user/home/slider/index.jsx
+++ src/pages/user/home/slider/index.jsx
@@ -1,82 +1,58 @@
-import React from 'react'
-import { Navigation } from 'swiper/modules'
-import { Swiper, SwiperSlide } from 'swiper/react'
+import React from "react";
+import { Autoplay, Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 export default function Slider() {
   return (
-    <section className="slider-swiper">
-    <Swiper
-      slidesPerView={1}
-      spaceBetween={30}
-      loop={true}
-      // autoplay={{
-      //   delay: 2500,
-      //   disableOnInteraction: false,
-      // }}
-      // pagination={{
-      //   clickable: true,
-      // }}
-      navigation={true}
-      modules={[ Navigation]}
-      className="mySwiper"
-      
-    >
-      <SwiperSlide className="slide1">
-        <div className="backgroud-slider">
-          <img
-            src="/images/electronics-slideshow_5_1920x.webp"
-            alt=""
-            className="slide-image"
-          />
-          <div className="background-content">
-            <div className="slider-left">Ipad pro</div>
-            <div>
-              Making them suitable for tasks such as note-taking, drawing,
-              and graphics design
-            </div>
+    <section className="slider-swiper h-[600px] w-full">
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        // autoplay={{
+        //   delay: 2500,
+        //   disableOnInteraction: false,
+        // }}
+        navigation={true}
+        modules={[Navigation]}
+        className="mySwiper w-full h-full"
+      >
+        <SwiperSlide>
+          <div
+            className="bg-black w-full h-full bg-cover bg-center"
+            style={{ backgroundImage: "url('https://cdn2.cellphones.com.vn/x/media/wysiwyg/1-Pro.jpg')" }}
+          >
+            {/* Your content here */}
           </div>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="backgroud-slider">
-          <img
-            src="/images/electronics-slideshow_1_1920x.webp"
-            alt=""
-            className="slide-image"
-          />
-          <div className="background-content">
-            <div className="slider-left">Ipad pro</div>
-            <div>
-              Making them suitable for tasks such as note-taking, drawing,
-              and graphics design
-            </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className="bg-black w-full h-full bg-cover bg-center"
+            style={{ backgroundImage: "url('https://marithe-official.com/main_banner/24FW/011_OHJAEHUN/pc_1.jpg')" }}
+          >
+            {/* Your content here */}
           </div>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-      <div className="backgroud-slider">
-          <img
-            src="/images/electronics-slideshow_1_1920x.webp"
-            alt=""
-            className="slide-image"
-          />
-          <div className="background-content">
-            <div className="slider-left">Ipad pro</div>
-            <div>
-              Making them suitable for tasks such as note-taking, drawing,
-              and graphics design
-            </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className="bg-black w-full h-full bg-cover bg-center"
+            style={{ backgroundImage: "url('https://marithe-official.com/main_banner/24FW/011_OHJAEHUN/pc_1.jpg')" }}
+          >
+            {/* Your content here */}
           </div>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      <SwiperSlide>Slide 5</SwiperSlide>
-      <SwiperSlide>Slide 6</SwiperSlide>
-      <SwiperSlide>Slide 7</SwiperSlide>
-    </Swiper>
-  </section>
-  )
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className="bg-black w-full h-full bg-cover bg-center"
+            style={{ backgroundImage: "url('https://marithe-official.com/main_banner/24FW/011_OHJAEHUN/pc_1.jpg')" }}
+          >
+            {/* Your content here */}
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </section>
+  );
 }
